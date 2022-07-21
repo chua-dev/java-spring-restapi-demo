1609a3363a6a832966e957ce271bcaa1ada984b4
dev-chua <chua3940@gmail.com>
2022-07-21 19:12:59 +0800

    controller, service, model

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @SpringBootApplication
-@RestController // This annotation make this class to serve as Rest endpoint
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping // First Rest Api
-	public String hello() {
-		return "Hello World";
-	}
+
 
 }
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController // This annotation make this class to serve as Rest endpoint
+@RequestMapping(path = "api/v1/student") // Extended API ROUTES
+// All Resources for API
+public class StudentController {
+
+    // Class Attribute
+    private final StudentService studentService;
+
+    // Constructor
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping // First Rest Api
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+}
